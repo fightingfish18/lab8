ed01c6f046b777054b0905c787a671b39f43b6db
William Smyth May <wsmay1@uw.edu>
2013-11-18 00:42:48 -0800

    Final page ready to turn in.

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,59 @@
+//William Smyth May
+//2013-11-18
+//This is the JS that prepares the movie shopping page
+
 /* controller.js
     Controller for Shopping Cart page
 */
 
-$(function(){
+$(function(){ //document ready function, prepares page
+	var formatLabels = {
+		dvd: 'DVD',
+		bluray: 'Blu-Ray'
+	};
+	
+	var cartModel = createCartModel();
+	
+	var cartView = createCartView({
+		model: cartModel,
+		template: $('.cart-item-template'),
+		container: $('.cart-items-container'),
+		totalPrice: $('.total-price')
+	});
+	
+	var cartJSON = localStorage.getItem('cart'); //Checks for stored cart on local storage
+	if (cartJSON && cartJSON.length > 0) {
+		cartModel.setItems(JSON.parse(cartJSON));
+	}
+	
+	var moviesModel = createMoviesModel({ //Pulls the list of movies for the page
+		url: 'https://courses.washington.edu/info343/ajax/movies/'
+	});
+	
+	var moviesView = createMoviesView({
+		model: moviesModel,
+		template: $('.movie-template'),
+		container: $('.movies-container')
+	});
+	
+	//refresh to get movies from server
+	moviesModel.refresh();
+	
+	moviesView.on('addToCart', function(data){ //function to add item to the shopping cart
+		var movie = moviesModel.getItem(data.movieID);
+		if (!movie)
+			throw 'Invalid movie ID "' + movieID + '"!'; 
 
+		cartModel.addItem({
+			id: movie.id,
+			title: movie.title,
+			format: data.format,
+			formatLabel: formatLabels[data.format],
+			price: movie.prices[data.format]
+		});
+	}); //addToCart event
+	cartModel.on('change', function(){
+		localStorage.setItem('cart', cartModel.toJSON());
+	});
 }); //doc ready()
 
